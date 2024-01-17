691dccaddc5bf4389ae051c72d9c805e2a8d4f95
Rahul Mandal <85156165+rahulmandalgg@users.noreply.github.com>
2024-01-17 16:12:36 +0530

    Update Client.py

--- Assignment-1/Client.py
+++ Assignment-1/Client.py
@@ -1,1 +1,19 @@
+import asyncio
+import aiohttp
 
+async def send_request(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.text()
+
+async def main():
+    tasks = []
+    for i in range(10000):
+        task = asyncio.create_task(send_request("http://localhost:5000/home"))
+        tasks.append(task)
+
+    results = await asyncio.gather(*tasks)
+    print(results)
+
+if __name__ == '__main__':
+    asyncio.run(main())
